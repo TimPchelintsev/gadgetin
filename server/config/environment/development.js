046196f7c1681d9fc9ef6399d5db261f143a110b
Timofey Pchelintsev <tpchelintsev@gmail.com>
2015-01-18 14:38:22 +0200

    Initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    // uri: 'mongodb://localhost/gadgetinapp-dev'
+    uri: 'mongodb://gadgetin:gadgetin@ds053370.mongolab.com:53370/gadgetin-dev'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    // uri: 'mongodb://localhost/gadgetinapp-test'
+    uri: 'mongodb://gadgetin:gadgetin@ds061200.mongolab.com:61200/gadgetin-test'
+  }
+};
