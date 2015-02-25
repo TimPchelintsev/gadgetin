60e22fde4734d3c0d4312e814495516300587d20
Timofey Pchelintsev <tpchelintsev@gmail.com>
2015-02-25 23:23:05 +0200

    Change view of users on people page

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -16,7 +16,7 @@ angular.module('gadgetinApp')
         })
         .then( function() {
           // Account created, redirect to home
-          $location.path('/');
+          $location.path('/profile');
         })
         .catch( function(err) {
           err = err.data;
