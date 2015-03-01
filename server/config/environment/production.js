3d3170252230be5f194d9148344a6d85a1854228
Timofey Pchelintsev <tpchelintsev@gmail.com>
2015-03-01 16:19:20 +0200

    change mongodb to mongolab in prod conf

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -11,13 +11,14 @@ module.exports = {
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            8080,
+            9000,
 
   // MongoDB connection options
   mongo: {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/gadgetinapp'
+            // 'mongodb://localhost/gadgetinapp'
+            'mongodb://gadgetin:gadgetin@ds053370.mongolab.com:53370/gadgetin-dev'
   }
 };
